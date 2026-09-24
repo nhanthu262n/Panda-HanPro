b6ae26071cd47f7b3c9ac8d433306028e33ea63e
nhanthu262n <nhanthu262n@gmail.com>
2026-09-24 21:19:18 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // v57.2-phonetics-sync-deep-feedback-20260924
-const CACHE_NAME = "pantutor-runtime-v57-2-20260924";
+const CACHE_NAME = "pantutor-runtime-v59-next-best-action";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
