835535b6d343974e31d30bd4175e6e023d83b8de
nhanthu262n <nhanthu262n@gmail.com>
2026-09-25 00:00:11 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // v57.2-phonetics-sync-deep-feedback-20260924
-const CACHE_NAME = "pantutor-runtime-v60-3-teacher-monitor";
+const CACHE_NAME = "pantutor-runtime-v60-4-original-fonts";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
